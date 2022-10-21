8c7674efb97387f6d98db3944622c627bac9903f
AFaidley <alviva.faidley@gmail.com>
2022-10-20 19:11:28 -0600

    Started working on thought controller, need help with figuring out .then issue

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,1 +1,47 @@
 // getThought, getOneThought, createAThought, updateAThought, deleteAThought, addAReaction, removeAReaction 
+
+const { User, Thought, Reaction } = require('../models')
+
+module.exports = {
+    getThought(req, res) {
+        Thought.find()
+            .then((thoughts) => res.json(thoughts))
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
+
+    getOneThought(req, res) {
+        Thought.findOne({ _id: req.params.thoughtId })
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: 'Unable to find a thought with a matching Id' })
+                    : res.json(thought)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+
+    createAThought(req, res) {
+        Thought.create(req.body)
+            .then((thought) => {
+                return User.findOneAndUpdate(
+                    { _id: req.body.userId },
+                    { $addToSet: { thoughts: thought._id } },
+                    { runValidators: true, new: true }
+                );
+            })
+            // Need to ask TA or tutor about the arg
+            .then((user) =>
+                !user
+                    ? res.status(404).json({
+                        message: 'Unable to find a user with a matching Id',
+                    })
+                    : res.json('New thought added successfully')
+            )
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
+}
